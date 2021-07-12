e70410aa63dde0fa4eb4d713d2fb3dcc9e54dc80
Your Name <lbookatz@hotmail.com>
2021-07-12 14:04:02 +0100

    plant changes requested

--- back-end/src/Plant/plant.controller.js
+++ back-end/src/Plant/plant.controller.js
@@ -2,7 +2,7 @@ const {Plant} = require('./plant.model')
 
 exports.getPlant = async (req,res) => {
     try {
-        res.status(200).send(req.plant)
+        res.status(200).json(req.plant)
     } catch (error) {
         res.status(500).send({message:"Plant not found"})
     }
@@ -11,7 +11,7 @@ exports.getPlant = async (req,res) => {
 exports.createPlant = async (res,req) => {
     try {
         const plant = new Plant(req.body)
-    } catch (error) {
-        
+    } catch (error) {        
+        res.status(500).send({message:"Plant not created"})
     }
 }
