8458d3d8749ef5b126c7973baad04a9d1afbce3f
Becca Mason <beccamason28@gmail.com>
2021-07-12 20:03:02 +0100

    #15 Created footer and worked on header

--- front-end/src/components/header/header.js
+++ front-end/src/components/header/header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <div className={"header"}>
             <div className={"menu"}>
-                {/* <img className={"header-logo"} src={Logo} alt="website logo" /> */}
+                <img className={"header-logo"} src={Logo} alt="website logo" />
                 <div className={"navbar"}>
                     <Router>
                         <Link className="link" to="/register">
@@ -25,7 +25,7 @@ const Header = () => {
                     </Router>
                 </div>
             </div>
-            {/* <p>Gardenless Greenery for the Concrete Jungle</p> */}
+            <p>Gardenless Greenery for the Concrete Jungle</p>
         </div>
     )
 }
