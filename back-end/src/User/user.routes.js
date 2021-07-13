f9d7af491c3dc296bc002348e4beba59d013d9ea
Your Name <lbookatz@hotmail.com>
2021-07-13 21:14:41 +0100

    got the login to work

--- back-end/src/User/user.routes.js
+++ back-end/src/User/user.routes.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken")
 
 
 userRouter.post("/", async (req, res) => {
+  console.log("test")
    try {
     const token = jwt.sign({ name: req.body.name }, process.env.SECRET)
     const user = await User.create({
@@ -28,7 +29,7 @@ userRouter.post("/", async (req, res) => {
 userRouter.get("/",  async (req,res) => {
   try {
     console.log(req.body)
-    const user = await User.findOne({})
+    const user = await User.find({})
       res.status(200).send(user)
   } catch (error) {
       res.status(500).send({message:"User not found"})
