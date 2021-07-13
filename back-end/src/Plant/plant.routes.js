7983172b0c97a90f424b95b0d9d2c8f5cf5b0782
Your Name <lbookatz@hotmail.com>
2021-07-13 13:37:31 +0100

    updated the userbackend

--- back-end/src/Plant/plant.routes.js
+++ back-end/src/Plant/plant.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { getPlant } = require("./plant.controller");
 const { user } = require("./plant.model");
-// const { auth } = require("../middleware")
+const { auth } = require("../middleware")
 const plantRouter = express.Router();
 
-plantRouter.get("/plant", getPlant);
+plantRouter.get("/plant", auth, getPlant);
 plantRouter.post("/plant");
 
 module.exports = {
