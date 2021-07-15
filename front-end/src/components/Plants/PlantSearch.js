983fab596fd7ebb4cbdaa059bb4055a31dbf0966
Becca Mason <beccamason28@gmail.com>
2021-07-15 18:38:04 +0100

    #59 plant search with bugs - submitted for FR to review

--- front-end/src/components/Plants/PlantSearch.js
+++ front-end/src/components/Plants/PlantSearch.js
@@ -6,8 +6,9 @@ const FetchPlants = () => {
     // const petSearch = "pets";
     // const maintSearch = "maintenance"; 
     // const lightSearch = "lighting"; 
-    const [page, setPage] = useState(allPlants);
+    const [page, setPage] = useState();
     const [plantjson, setPlantJson] = useState([]);
+    const [formInput, setFormInput] = useState(); 
     const renderAll = async() => {
         try {
             const fetchAll = await fetch("http://localhost:5000/plant")
@@ -17,8 +18,35 @@ const FetchPlants = () => {
         } catch (err) {
             console.log(err);
         }
+    };
+    // const renderPets = async (testing) => {
+    //     const response = await fetch("http://localhost:5000/plant"), {
+    //     method: "POST", 
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({
+    //         petFriendly: testing
+    //     })
+    //     }
+    //     const data = await response.json()
+    //     setPlantJson(data);
+    // }
+    const renderSearch = async (formInput) => {
+        const response = await fetch("http://localhost:5000/plant", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                name: formInput
+            })
+        })
+        const data = await response.json()
+        console.log(data);
+        setPlantJson(data);
+        // console.log(plantjson);
+    }
 
-
+    const getPlants = (e) => {
+        e.preventDefault(); 
+        renderSearch(formInput)
     }
 
     const navigatePage = (nextPage) => {
@@ -26,16 +54,16 @@ const FetchPlants = () => {
     }
 
 
-
     return (
         <div className={"plantheading"}>
             <header>
             <button className={"plantbutton"} onClick={() => navigatePage(renderAll)}>All Plants</button>
         </header>
-        <p>{plantjson.map(plantjson => <div>Name: {plantjson.name}, Maintenance: {plantjson.maintenance}</div>)}</p>
-        {/* <div>
-            <button className={"optionButton"} onClick={getPets}
-        </div> */}
+        <form onSubmit={getPlants}>
+            <input onChange={(e) => setFormInput(e.target.value)} />
+            <button type="submit" />
+        </form>
+        <p>{plantjson.map(plantjson => <p>Name: {plantjson.name}, Maintenance: {plantjson.maintenance}</p>)}</p>
         {page === allPlants && renderAll}
         </div>
     )
