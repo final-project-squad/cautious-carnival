55411a35284a39e3eab80baee3365797c324719d
Fernanda Ricciardi <74388130+FernandaRicciardi@users.noreply.github.com>
2021-07-21 19:29:17 +0100

    #125 Plant Planner style is done

--- front-end/src/components/todolist/Todo.js
+++ front-end/src/components/todolist/Todo.js
@@ -28,20 +28,23 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo}) {
     <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
      key={index}
     >
-
-        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-            {todo.text}
+        <div className="todo-list">
+            <div className="todo-item" key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}
+            </div>
+        
+            <div className='icons'>
+                <IoIosCloseCircleOutline 
+                onClick={() => removeTodo(todo.id)}
+                className='delete-icon'
+                />
+                <FiEdit 
+                onClick={() => setEdit({id: todo.id, value: todo.text})}
+                className='edit-icon'
+                />
+            </div>
         </div>
-        <div className='icons'>
-            <IoIosCloseCircleOutline 
-            onClick={() => removeTodo(todo.id)}
-            className='delete-icon'
-            />
-            <FiEdit 
-            onClick={() => setEdit({id: todo.id, value: todo.text})}
-            className='edit-icon'
-            />
-        </div>
+        
 
 </div>
 ));
